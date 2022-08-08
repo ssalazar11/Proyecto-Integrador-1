da6eae8ca96df6ab632a77282f15fd5e103d00de
Samuel Salazar <sa.salazarasalazar@gmail.com>
2022-08-07 19:52:03 -0500

    Conexion a base de datos

--- proyecto1/aplicacionp1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Aplicacionp1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aplicacionp1'
--- proyecto1/proyecto1/settings.py
+++ proyecto1/proyecto1/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'proyecto1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME':'InOut',
     }
 }
 

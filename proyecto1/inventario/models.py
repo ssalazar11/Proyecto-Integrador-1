071c826fa7aa89e1989288b9d71577c663dd9d21
Samuel Salazar <sa.salazarasalazar@gmail.com>
2022-08-16 08:43:34 -0500

    Cambio a base de datos sqlite

--- proyecto1/inventario/models.py
+++ proyecto1/inventario/models.py
@@ -3,14 +3,18 @@ from django.db import models
 # Create your models here.
 
 class usuario(models.Model):
-    name=models.CharField(max_length=60)
+    name=models.CharField(max_length=60, null=True)
+    apellido=models.CharField(max_length=30, null=True)
+    usuario=models.CharField(max_length=30, null=True)
+    correo=models.CharField(max_length=30, null=True)
+    constraseña=models.CharField(max_length=20, null=True)
     nombreCompania=models.CharField(max_length=60, null=True)
 
 class compania(models.Model):
     nombreCompania=models.OneToOneField(usuario, on_delete=models.CASCADE, primary_key=True)
 
 class producto(models.Model):
-    nombreProducto=models.CharField(max_length=60)
+    nombreProducto=models.CharField(max_length=60, null=True)
     tipoProducto=models.IntegerField()
     cantidadProducto=models.IntegerField()
 

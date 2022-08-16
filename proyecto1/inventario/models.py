689068d5ee190a722e8cdef75b393ae07232dd2a
Samuel Salazar <sa.salazarasalazar@gmail.com>
2022-08-16 09:34:50 -0500

    arreglo 2

--- proyecto1/inventario/models.py
+++ proyecto1/inventario/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+
+class compania(models.Model):
+    nombreCompania=models.CharField(max_length=50, null=True)
+
 class usuario(models.Model):
     name=models.CharField(max_length=60, null=True)
     apellido=models.CharField(max_length=30, null=True)
@@ -9,9 +13,6 @@ class usuario(models.Model):
     correo=models.CharField(max_length=30, null=True)
     constraseña=models.CharField(max_length=20, null=True)
     nombreCompania=models.OneToOneField(compania, on_delete=models.CASCADE, primary_key=True)
-
-class compania(models.Model):
-    nombreCompania=models.CharField(max_length=50, null=True)
 
 class producto(models.Model):
     nombreProducto=models.CharField(max_length=60, null=True)

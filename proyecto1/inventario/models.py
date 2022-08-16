48b4eb32a22dabdd2ef7d06f72489daff4045a48
Ortegago3 <Hortegag@eafit.edu.co>
2022-08-16 09:15:13 -0500

    correcciones bases de datos

--- proyecto1/inventario/models.py
+++ proyecto1/inventario/models.py
@@ -8,3 +8,11 @@ class usuario(models.Model):
     usuario = models.CharField(max_length=20)
     correo = models.CharField(max_length=100)
     clave = models.CharField(max_length=100)
+
+class compania(models.Model):
+    nombreCompania = models.OneToOneField(usuario, on_delete=models.CASCADE,primary_key = True)
+
+class producto(models.Model):
+    nombreProducto = models.CharField(max_length=60, null= True)
+    tipoProducto = models.IntegerField()
+    cantidadProducto = models.IntegerField()

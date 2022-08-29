c2422f30a54669d547d2ef057dd91fe8be5add2a
Julian Giraldo Perez <jgpbak@outlook.com>
2022-08-29 17:28:13 -0500

    Creacion de links entre html

--- InOut/settings.py
+++ InOut/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'InOut_app',
+    'Productos',
 ]
 
 MIDDLEWARE = [
--- InOut_app/urls.py
+++ InOut_app/urls.py
@@ -5,11 +5,12 @@ urlpatterns = [
     path('Registro/', views.Registro, name="Registro"),
     path('Ingreso/', views.Ingreso, name="Ingreso"),
     path('Home/',   views.Home, name="Home"),
-    path('Usuario/', views.Usuario, name="Usuario"),
+    path('Usuario/', views.Usuario, name="Usuarios"),
     path('Productos/', views.Productos, name="Productos"),
     path('Graficas/', views.graficas, name="Graficas"),
     path('Tendencias/', views.Tendencias, name="Tendencias"),
     path('Opciones/', views.Opciones, name="Opciones"),
     path('Main/', views.Main, name="Main"),
-    path('Welcome/', views.Welcome, name="Welcome")
+    path('Welcome/', views.Welcome, name="Welcome"),
+    path('Logout/', views.Welcome, name="Logout")
 ]

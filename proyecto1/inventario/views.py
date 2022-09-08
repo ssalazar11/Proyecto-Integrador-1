5a982ecc6304283b3e59fc6009cbc502bdefd570
Samuel Salazar <sa.salazarasalazar@gmail.com>
2022-09-08 12:51:02 -0500

    creacion de registro y borrado de productos

--- proyecto1/inventario/views.py
+++ proyecto1/inventario/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from inventario import models
 # Create your views here.
+
+def registroProducto(request):
+    if request.method == 'POST':
+        nombreProducto=request.POST['nombreProducto']
+        tipoProducto=request.POST['tipoProducto']
+        cantidadProducto=request.POST['cantidadProducto']
+        agregar=models.producto(nombreProducto=nombreProducto, tipoProducto=tipoProducto, cantidadProducto=cantidadProducto)
+        agregar.save()
+
+def eliminacionProducto(request, nombre):
+    if request.method == 'POST':
+        producto=producto.objects.get(nombreProducto=nombre)
+        producto.delete()
+        
 
 def producto(request):
     return render(request, "producto.html")

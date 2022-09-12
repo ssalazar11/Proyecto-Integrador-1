a8e1efeefbfa979e70e02388d08ac50e948a4e3f
Samuel Salazar <sa.salazarasalazar@gmail.com>
2022-09-12 14:37:32 -0500

    Creación pag admin y funcionalidad de añadir

--- proyecto1/inventario/admin.py
+++ proyecto1/inventario/admin.py
@@ -1,3 +1,6 @@
+from math import prod
 from django.contrib import admin
-
+from .models import producto
 # Register your models here.
+
+admin.site.register(producto)
--- proyecto1/inventario/models.py
+++ proyecto1/inventario/models.py
@@ -2,20 +2,22 @@ from django.db import models
 
 # Create your models here.
 class compania(models.Model):
-    nombreCompania=models.CharField(max_length=50, null=True)
+    nombreCompania=models.CharField(max_length=50, primary_key=True)
     
 
 class usuario(models.Model):
-    name=models.CharField(max_length=60, null=True)
+    name=models.CharField(max_length=60, primary_key=True)
     apellido=models.CharField(max_length=30, null=True)
     usuario=models.CharField(max_length=30, null=True)
     correo=models.CharField(max_length=30, null=True)
     constraseña=models.CharField(max_length=20, null=True)
-    nombreCompania=models.OneToOneField(compania, on_delete=models.CASCADE, primary_key=True)
+    nombreCompania=models.OneToOneField(compania, on_delete=models.CASCADE)
 
 class producto(models.Model):
-    nombreProducto=models.CharField(max_length=60, null=True)
-    tipoProducto=models.IntegerField()
+    nombreProducto=models.CharField(max_length=60, primary_key=True)
+    tipoProducto=models.CharField(max_length=30)
     cantidadProducto=models.IntegerField()
+    def __str__(self):
+        return self.nombreProducto
 
     
